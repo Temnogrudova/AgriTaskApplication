7e106374fba42fef36853627013d2443a4287bc6
ekaterinatemnogrudova <ekaterina@fintier.com>
2019-07-17 17:00:13 +0300

    removed unused libs

--- app/src/main/java/com/agritask/ekaterinatemnogrudova/agritask/models/Item.java
+++ app/src/main/java/com/agritask/ekaterinatemnogrudova/agritask/models/Item.java
@@ -2,12 +2,9 @@ package com.agritask.ekaterinatemnogrudova.agritask.models;
 
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
-import com.google.gson.annotations.SerializedName;
 
 public class Item {
-    @SerializedName("resId")
     private int resId;
-    @SerializedName("id")
     private int id;
 
     public int getResId() {
